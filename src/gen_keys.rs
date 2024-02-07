d3bf8b72cc6ed3206d9e213b2795f4e87ec84d91
Levent DEMIR <levent.demir@zama.ai>
2024-02-07 12:08:08 +0100

    chore(tfhe-rs): bump to v0.5.1

--- src/gen_keys.rs
+++ src/gen_keys.rs
@@ -4,14 +4,12 @@
 // All rights reserved.
 
 use tfhe::{
-    generate_keys, shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS, ClientKey,
-    CompactPublicKey, ConfigBuilder, ServerKey,
+    generate_keys, shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS, ClientKey, CompactPublicKey, ConfigBuilder, ServerKey
 };
 
 pub fn gen_keys() -> (ClientKey, ServerKey, CompactPublicKey) {
-    let config = ConfigBuilder::all_disabled()
-        .enable_custom_integers(PARAM_MESSAGE_2_CARRY_2_KS_PBS, None)
-        .build();
+    let config = ConfigBuilder::with_custom_parameters(PARAM_MESSAGE_2_CARRY_2_KS_PBS, None)
+         .build();
     let (cks, sks) = generate_keys(config);
     let pks = CompactPublicKey::new(&cks);
     (cks, sks, pks)
